1caf6f9fb323e4204d64d90e05deef53553f2f9b
DhiaElhak-Ezzeddini <dhiaelhak.ezzeddini@supcom.tn>
2025-11-20 16:42:07 +0100

    Add setup_env_vars.ps1: PowerShell script to configure nnU-Net environment variables

---
+++ Data Preparation/setup_env_vars.ps1
@@ -0,0 +1,8 @@
+$env:nnUNet_raw = "D:\3D-Segmentation-and-Modeling-of-Lower-Limb-Bones\Data Preparation\nnUNet_raw_data"
+$env:nnUNet_preprocessed = "D:\3D-Segmentation-and-Modeling-of-Lower-Limb-Bones\Data Preparation\nnUNet_preprocessed"
+$env:nnUNet_results = "D:\3D-Segmentation-and-Modeling-of-Lower-Limb-Bones\Data Preparation\nnUNet_results"
+
+Write-Host "nnU-Net environment variables set:"
+Write-Host "  nnUNet_raw: $env:nnUNet_raw"
+Write-Host "  nnUNet_preprocessed: $env:nnUNet_preprocessed"
+Write-Host "  nnUNet_results: $env:nnUNet_results"
